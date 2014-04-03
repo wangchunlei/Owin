363ba58844edd8567fc9cc19a8e7f26d6d2cca3e
wangchunlei <wcl_orange@163.com>
2014-04-03 22:14:17 +0800

    owin

---
+++ SignalRSelfHost/SignalRSelfHost/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRSelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://*:8088";
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine("Server running on {0}", url);
+                Console.ReadLine();
+            }
+        }
+    }
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+    public class MyHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addMessage(name, message);
+        }
+    }
+}
