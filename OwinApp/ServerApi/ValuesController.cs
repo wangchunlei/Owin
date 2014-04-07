bd8cb48d853ff8dea7a4874e8fadc19dde5b0c78
wangchunlei <wcl_orange@163.com>
2014-04-07 19:21:17 +0800

    commit

---
+++ OwinApp/ServerApi/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ServerApi
+{
+    public class ValuesController : ApiController
+    {
+        public IEnumerable<string> GetValues()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        public string GetValue()
+        {
+            return "value";
+        }
+
+        public void PostValue([FromBody]string value)
+        {
+            
+        }
+    }
+}
