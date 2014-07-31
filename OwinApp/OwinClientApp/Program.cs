209da92091f19c16c0f1b81e0ee2d0efb5039784
wangchunlei <wcl_orange@163.com>
2014-07-31 15:34:29 +0800

    cc

--- OwinApp/OwinClientApp/Program.cs
+++ OwinApp/OwinClientApp/Program.cs
@@ -1,22 +1,33 @@
-﻿using RestSharp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;
+using System.Net;
 using System.Threading.Tasks;
+using RestSharp;
 
 namespace OwinClientApp
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            GetRequestToken();
+            Console.ReadKey(false);
         }
 
         private static void GetRequestToken()
         {
-            var client = new RestClient("http://localhost:8003/");
-            
+            var client =
+                new RestClient(
+                    "https://192.168.70.132:8060//api/BaseCaseCaseObjectAPI/GetSetting?code=pagecounterconfig&usercode=");
+            ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+            var request = new RestRequest(Method.POST);
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    var response = client.Execute(request);
+                    Console.WriteLine(DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff") + ":" + response.Content);
+                });
+            }
             //client.Authenticator = new OAuth2AuthorizationRequestHeaderAuthenticator()
         }
     }
